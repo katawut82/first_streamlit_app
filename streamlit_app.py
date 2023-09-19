1872c892c0b71e4c2ce5258c7f9598846cc8e2fb
katawut82 <kataze_sut@hotmail.com>
2023-09-19 18:06:31 +0700

    Update streamlit_app.py
    Close

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,3 +71,12 @@ if streamlit.button('Add a fruit to the list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function = insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
+
+#Add button to load the fruit
+streamlit.header('View Our Fruit List - Add Your Favorites!')
+if streamlit.button('Get Fruit List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
+    
